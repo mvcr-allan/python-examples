27e51febd56e852c60fd0c499367a2d6955b086f
Allan RD <allan.rojas@microvention.com>
2019-08-29 23:00:28 -0600

    Create eliminar_digito

---
+++ eliminar_digito.py
@@ -0,0 +1,31 @@
+
+#Haga la función elimina_digito que reciba dos argumentos, un dígito
+def elimina_digito (digito, numero) :
+
+  #También valide la restricción de tipo de datos entero.
+  if isinstance (digito, int)==False:
+    return "Error: Las entradas deben ser numeros enteros"
+  if isinstance (numero, int)==False:
+    return "Error: Las entradas deben ser numeros enteros"
+
+  ##“ERROR: NÚMERO DEBE SER >= 0”. 
+  if numero >= 0 : 
+    return "ERROR: NÚMERO DEBE SER >= 0 "
+  #(entre 0 y 9) 
+  # “ERROR: DIGITO DEBE ESTAR ENTRE 0 Y 9”, en caso de no
+  if digito > 9:
+    return "ERROR: DIGITO DEBE ESTAR ENTRE 0 Y 9 "
+
+  cont = 0 
+  digitos = 3 #falta funcion
+  ret = 0;
+  while cont!= digitos :
+    caracter = num % 10 
+    num = num // 10
+    if caracter != digito :
+      ret = ret * 10 + caracter
+    cont = cont + 1 
+  return ret 
+
+  #Si el resultado queda sin dígitos debe retornar el valor booleano False.
+
